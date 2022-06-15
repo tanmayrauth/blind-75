fd33783e212c7c72df7e24f3a3fb620cc5fe6414
Tanmay Rauth <tanmayrauth@gmail.com>
2022-06-15 13:32:04 +0000

    change

--- container_with_most_water.py
+++ container_with_most_water.py
@@ -8,9 +8,8 @@ Now for next cycle the logic is that you will remove the one which had less heig
 class Solution:
     def maxArea(self, height: List[int]) -> int:
         
-        left      = 0
-        right     = len(height)-1
-        max_area  = 0
+        left, right = 0, len(height)-1 
+        max_area    = 0
         
         while left < right:
             if height[left] < height[right]:
