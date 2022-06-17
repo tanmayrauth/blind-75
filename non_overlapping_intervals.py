1dfd3cfc5e6f025c809e0bb8fa78ab8d5e0d6de1
Tanmay Rauth <tanmayrauth@gmail.com>
2022-06-17 12:21:09 +0000

    update

--- non_overlapping_intervals.py
+++ non_overlapping_intervals.py
@@ -1,6 +1,5 @@
 """
-The logic is very much similar to merge interval problem. Just instead of output array we keep track of elements in prevEnd variable.
-Loop + IF Else
+The logic is very much similar to merge interval problem (Loop + IF Else). Just instead of output array we keep track of elements in prevEnd variable.
 
 Sorting is must and saving the PrevEnd value is also imp because using that only we determine whether our current bar is overlapping or not. 
 Along with it we update the prevEnd with the min across currentEnd and prevEnd because we want to minimise overlapping. 
