d0ea7a37658ab018f4889c48e84a22616a49f44b
Tanmay Rauth <tanmayrauth@gmail.com>
2022-06-19 04:18:58 +0000

    pallindrome

---
+++ valid_pallindrome.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s1 = []
+        for key in s:
+            if key.isalpha() or key.isdigit():
+                s1.append(key.lower())
+        
+        return s1 == s1[::-1] 
