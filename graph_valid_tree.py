3e5e677c8d2c74f4f3fde04d4743dc991971ee12
Tanmay Rauth <tanmayrauth@gmail.com>
2022-06-22 10:32:05 +0000

    solution

--- graph_valid_tree.py
+++ graph_valid_tree.py
@@ -1,4 +1,15 @@
+"""
+This is a good problem for graph. Don't ignore it. 
 
+First you will create a adj node dict. 
+Now since this is undirected graph so while doing dfs you need to keep track of prevNode jst to see if it creates cycle or not. 
+Your base condition for dfs should be that if node in visit then return False. 
+Then you should run loop over adj node and see if any of it is in prevNode then continue it else return False. 
+At the end you also need to make sure that your dfs return True and the length of visit set is equal to the total no of graph nodes. 
+This will ensure the whole graph is connected. 
+
+Compare this problem with Course Schedule problem you will find some differences. And check the learning section.
+"""
 
 def Solution:
     def validTree(self, n, edges):
